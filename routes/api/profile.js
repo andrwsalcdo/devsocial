011b6c50b845c990b7e929a49eafa89da4a671e0
andrwsalcdo <asalcedo07@gmail.com>
2018-04-28 20:30:31 +0200

    add a test route for profile

---
+++ routes/api/profile.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+/*
+    @route  GET api/profile/test
+    @desc   Tests profile route
+    @access public
+*/
+router.get('/test', (req, res) => res.json({ msg: 'profile test route' }));
+
+module.exports = router;
