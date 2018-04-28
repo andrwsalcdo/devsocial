0764c05ed79ada6eb6f9fc81e71ade5e27cac919
andrwsalcdo <asalcedo07@gmail.com>
2018-04-28 20:31:38 +0200

    add & use routes in server.js & delete hello everyone message on index route

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+// Routes
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+
 const app = express();
 
 const db = require('./config/keys').mongoURI;
-
 // connect to mongoDB
 mongoose
   .connect(db)
   .then(() => console.log('mongodb connected...fireworks'))
   .catch(err => console.log('oh no! no fireworks...', err));
 
-// basic route for now
-app.get('/', (req, res) => res.send('Hello everyone'));
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
 
 const port = process.env.PORT || 5000;
 
