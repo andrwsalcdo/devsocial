a4b16579a0e6ec36f42780c4513d52a2bb5ba9f8
andrwsalcdo <asalcedo07@gmail.com>
2018-04-30 20:08:35 +0200

    add bodyParser & its middleware to server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 // Routes
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
 const profile = require("./routes/api/profile");
 
 const app = express();
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const db = require("./config/keys").mongoURI;
 // connect to mongodb
