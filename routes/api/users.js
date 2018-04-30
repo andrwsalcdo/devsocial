8875c5bbd4ff0a1dab597bdd14b13ea2b2fe2301
andrwsalcdo <asalcedo07@gmail.com>
2018-04-30 21:08:31 +0200

    create JWT on user login. jwt.sign(). tested on postman.

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../../models/User");
+const keys = require("../../config/keys");
 
 /*
     @route  GET api/users/test
@@ -63,9 +65,20 @@ router.post("/login", (req, res) => {
     }
     // password is same?
     bcrypt.compare(password, user.password).then(isMatch => {
+      // User logins successfully.
       if (isMatch) {
-        res.json({ msg: "user success" });
-        // todo: return JWT
+        const payload = { id: user.id, name: user.name, avatar: user.avatar };
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
       } else {
         return res.status(400).json({ password: "Password incorrect" });
       }
