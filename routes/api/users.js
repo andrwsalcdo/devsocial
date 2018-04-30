193013d7323a89aeb9f4c39b1b36642060c8557d
andrwsalcdo <asalcedo07@gmail.com>
2018-04-30 20:12:57 +0200

    add POST request when user registers. tested on postman

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const User = require("../../models/User");
 
 /*
     @route  GET api/users/test
@@ -7,5 +10,41 @@ const router = express.Router();
     @access Public
 */
 router.get("/test", (req, res) => res.json({ msg: "users test route" }));
+
+/*
+    @route  POST api/users/register
+    @desc   Register User
+    @access Public
+*/
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exits" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", // size
+        r: "pg", // rating
+        d: "mm" // default
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
