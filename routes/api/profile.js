f1fa7f58894707ca0a4e57e31764f98d05afe860
andrwsalcdo <asalcedo07@gmail.com>
2018-05-04 21:14:48 +0200

    add a GET profile by handle route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -58,6 +58,26 @@ router.get("/all", (req, res) => {
 });
 
 /*
+    @route  GET api/profile/handle/:handle
+    @desc   Get profile by handle
+    @access Public
+*/
+router.get("/handle/:handle", (req, res) => {
+  let errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There isn't a profile for this user";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+/*
     @route  POST api/profile
     @desc   Create or Edit User's Profile
     @access Private
