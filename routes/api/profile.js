4505ee5dbd73579db4c586142a504b78af079615
andrwsalcdo <asalcedo07@gmail.com>
2018-05-04 22:03:10 +0200

    add POST education to profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,7 @@ const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 const validateProfileInput = require("../../validation/profile");
 const validateExperienceInput = require("../../validation/experience");
+const validateEducationInput = require("../../validation/education");
 
 /*
     @route  GET api/profile/test
@@ -191,6 +192,40 @@ router.post(
 
       // add to front of experience array
       profile.experience.unshift(newExp);
+
+      profile.save().then(profile => res.json(profile));
+    });
+  }
+);
+
+/*
+    @route  POST api/profile/education
+    @desc   Add education to profile
+    @access Private
+*/
+router.post(
+  "/education",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateEducationInput(req.body);
+    //  check validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newEdu = {
+        school: req.body.school,
+        degree: req.body.degree,
+        fieldofstudy: req.body.fieldofstudy,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
+
+      // add to front of education array
+      profile.education.unshift(newEdu);
 
       profile.save().then(profile => res.json(profile));
     });
