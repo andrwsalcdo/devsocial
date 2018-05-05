0174a210e632a3c9fdee82c5f210970d79ddec89
andrwsalcdo <asalcedo07@gmail.com>
2018-05-05 22:15:34 +0200

    import components/layout/ into App.js. todo: React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Footer from "./components/layout/Footer";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>hello world</h1>
-      </div>
+      <React.Fragment>
+        <Navbar />
+        <Landing />
+        <Footer />
+      </React.Fragment>
     );
   }
 }
