462a68d06e49289e9ccb7a197c4d451092b8d88b
andrwsalcdo <asalcedo07@gmail.com>
2018-05-05 22:14:03 +0200

    add Footer. dumb component

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="bg-dark text-white mt-5 p-4 text-center">
+    Copyright &copy; {new Date().getFullYear()} DevSocial
+  </footer>
+);
+
+export default Footer;
