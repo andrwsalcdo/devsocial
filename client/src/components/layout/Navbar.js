f22e9cfc5b0241bc24f5decb2962aed43098b86e
andrwsalcdo <asalcedo07@gmail.com>
2018-05-05 22:13:32 +0200

    add Navbar. dumb component.

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+        <div className="container">
+          <a className="navbar-brand">DevSocial</a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mobile-nav"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link">Developers</a>
+              </li>
+            </ul>
+
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link">Sign Up</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">Login</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
