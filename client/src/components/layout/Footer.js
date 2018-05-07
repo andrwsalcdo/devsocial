e1c31817b75b107587c97d846f797908aa689913
andrwsalcdo <asalcedo07@gmail.com>
2018-05-07 18:20:01 +0200

    style: change <Footer> to sticky

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 const Footer = () => (
-  <footer className="bg-dark text-white mt-5 p-4 text-center">
-    Copyright &copy; {new Date().getFullYear()} DevSocial
+  <footer className="bg-dark text-white mt-5 p-4 text-center footer">
+    <div className="container">
+      Copyright &copy; {new Date().getFullYear()} DevSocial
+    </div>
   </footer>
 );
 
