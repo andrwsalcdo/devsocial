a703466346803fe765863ab6b811b7bb2a4ec903
andrwsalcdo <asalcedo07@gmail.com>
2018-05-07 15:34:23 +0200

    adjust <a> in Navbar to react-router-dom's <Link>.

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
-          <a className="navbar-brand">DevSocial</a>
+          <Link className="navbar-brand" to="/">
+            DevSocial
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -18,16 +21,22 @@ class Navbar extends Component {
           <div className="collapse navbar-collapse" id="mobile-nav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <a className="nav-link">Developers</a>
+                <Link className="nav-link" to="/profiles">
+                  Developers
+                </Link>
               </li>
             </ul>
 
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <a className="nav-link">Sign Up</a>
+                <Link className="nav-link" to="/register">
+                  Sign Up
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link">Login</a>
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
               </li>
             </ul>
           </div>
