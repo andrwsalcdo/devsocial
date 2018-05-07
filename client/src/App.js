21ee51fc583dc5a2775baab8f75583e17ce00636
andrwsalcdo <asalcedo07@gmail.com>
2018-05-07 15:29:45 +0200

    add BrowserRouter & set up Routes for landing,register,& login components in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Footer from "./components/layout/Footer";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Navbar />
-        <Landing />
-        <Footer />
-      </React.Fragment>
+      <BrowserRouter>
+        <React.Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </React.Fragment>
+      </BrowserRouter>
     );
   }
 }
