1ad92ccfa80e2d3f45113f2395efaded64b3c77e
andrwsalcdo <asalcedo07@gmail.com>
2018-05-08 17:24:33 +0200

    add loginUser action & adjust authReducer with SET_CURRENT_USER case

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -1,5 +1,8 @@
 import axios from "axios";
+import jwt_decode from "jwt-decode";
+import setAuthorizationHeader from "../../utils/setAuthorizationHeader";
 import { getErrors } from "./errorsActions";
+import { SET_CURRENT_USER } from "./actionTypes";
 
 // Register user
 export const registerUser = (userData, history) => dispatch => {
@@ -8,3 +11,27 @@ export const registerUser = (userData, history) => dispatch => {
     .then(res => history.push("/login"))
     .catch(err => dispatch(getErrors(err)));
 };
+
+// Login - Get User Token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      const { token } = res.data;
+      // send token to ls
+      localStorage.setItem("jwt-ds", token);
+      // set auth header
+      setAuthorizationHeader(token);
+      // decode token to get user data
+      const decodedUser = jwt_decode(token);
+      // set current user
+      dispatch(setCurrentUser(decodedUser));
+    })
+    .catch(err => dispatch(getErrors(err)));
+};
+
+// Set Logged in user
+export const setCurrentUser = decodedUser => ({
+  type: SET_CURRENT_USER,
+  payload: decodedUser
+});
--- client/src/redux/reducers/authReducer.js
+++ client/src/redux/reducers/authReducer.js
@@ -1,3 +1,6 @@
+import { SET_CURRENT_USER } from "../actions/actionTypes";
+import isEmpty from "../../utils/isEmpty";
+
 const initialState = {
   isAuthenticated: false,
   user: {}
@@ -5,6 +8,12 @@ const initialState = {
 
 function authReducer(state = initialState, action) {
   switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload
+      };
     default:
       return state;
   }
