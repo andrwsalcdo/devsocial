659a643e17120754798de487ce93902fcf73c441
andrwsalcdo <asalcedo07@gmail.com>
2018-05-08 14:40:14 +0200

    set up redux Provider in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Provider } from "react-redux";
+import store from "./redux/configureStore";
 import { BrowserRouter, Route } from "react-router-dom";
 import AppRouter from "./routes/AppRouter";
 
 const App = () => (
-  <BrowserRouter>
-    <Route component={AppRouter} />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Route component={AppRouter} />
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
