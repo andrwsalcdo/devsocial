ad511124641b362b586f45e9259a0d90e8227155
andrwsalcdo <asalcedo07@gmail.com>
2018-05-09 20:34:07 +0200

    add a select options utility data array

---
+++ client/src/utils/selectOptions.js
@@ -0,0 +1,14 @@
+// select options for the create-profile component
+const selectOptions = [
+  { label: "* Select Professional Status", value: 0 },
+  { label: "Developer", value: "Developer" },
+  { label: "Junior Developer", value: "Junior Developer" },
+  { label: "Senior Developer", value: "Senior Developer" },
+  { label: "Manager", value: "Manager" },
+  { label: "Student or Learning", value: "Student or Learning" },
+  { label: "Instructor or Teacher", value: "Instructor or Teacher" },
+  { label: "Intern", value: "Intern" },
+  { label: "Other", value: "Other" }
+];
+
+export default selectOptions;
