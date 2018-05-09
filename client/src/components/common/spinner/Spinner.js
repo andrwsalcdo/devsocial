26caac1bf8b9374f7375fd4bae6297e556a9e75f
andrwsalcdo <asalcedo07@gmail.com>
2018-05-09 17:34:32 +0200

    add a <Spinner /> with keyframe animations

---
+++ client/src/components/common/spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./styles.css";
+
+const Spinner = () => (
+  <div className="spinner-container">
+    <div className="spinner" />
+  </div>
+);
+
+export default Spinner;
