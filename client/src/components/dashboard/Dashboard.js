c22bc5f7ec0fb21a656c721aecd0ffedcaa9de4c
andrwsalcdo <asalcedo07@gmail.com>
2018-05-09 18:32:01 +0200

    set up <Dashboard />. todo: add user's profile data jsx

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../redux/actions/profileActions";
+import Spinner from "../common/spinner/Spinner";
 
 class Dashboard extends Component {
   static propTypes = {
@@ -11,15 +13,37 @@ class Dashboard extends Component {
   };
 
   componentDidMount() {
-    if (this.props.auth.isAuthenticated) {
-      this.props.getCurrentProfile();
-    }
+    this.props.getCurrentProfile();
   }
 
   render() {
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
     return (
-      <div>
-        <h1>dashboard</h1>
+      <div className="Dashboard">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4">Dashboard</h1>
+              {profile === null || loading ? (
+                <Spinner />
+              ) : // check if the logged in user has profile data
+              Object.keys(profile).length > 0 ? (
+                // TODO: add profile data
+                <h1>user profile data</h1>
+              ) : (
+                // user is logged in, but doesn't have a profile
+                <div>
+                  <p className="lead text-muted">Welcome {user.name} </p>
+                  <p>You have not setup a profile, please add some info</p>
+                  <Link to="/create-profile" className="btn btn-lg btn-info">
+                    Create Profile
+                  </Link>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
