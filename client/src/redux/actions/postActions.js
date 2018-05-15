75b3121a1c52b52a086b982dc22aac60efc59852
andrwsalcdo <asalcedo07@gmail.com>
2018-05-15 18:45:04 +0200

    create post_loading & get_posts redux actions

--- client/src/redux/actions/postActions.js
+++ client/src/redux/actions/postActions.js
@@ -1,10 +1,19 @@
 import axios from "axios";
-import { ADD_POST } from "./actionTypes";
+import { ADD_POST, POST_LOADING, GET_POSTS } from "./actionTypes";
 import { getErrors } from "./errorsActions";
 
 const _addPost = postData => ({
   type: ADD_POST,
   payload: postData
+});
+
+const _getPosts = data => ({
+  type: GET_POSTS,
+  payload: data
+});
+
+const _setPostLoading = () => ({
+  type: POST_LOADING
 });
 
 // Add Post
@@ -13,4 +22,13 @@ export const addPost = postData => dispatch => {
     .post("/api/posts", postData)
     .then(res => dispatch(_addPost(res.data)))
     .catch(err => dispatch(getErrors(err)));
+};
+
+// Get All Posts
+export const getAllPosts = () => dispatch => {
+  dispatch(_setPostLoading());
+  axios
+    .get("/api/posts")
+    .then(res => dispatch(_getPosts(res.data)))
+    .catch(err => dispatch(_getPosts(null)));
 };
