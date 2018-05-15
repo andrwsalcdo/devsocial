914e126c5a3a93876b6b07f5d26fbf86483df166
andrwsalcdo <asalcedo07@gmail.com>
2018-05-15 15:15:36 +0200

    create <ProfileGithub /> to display latest gh repos

---
+++ client/src/components/profile/ProfileGithub.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import keys from "../../config/keys";
+import fetchGithub from "../../utils/githubFetch";
+
+class ProfileGithub extends Component {
+  static propTypes = {
+    username: PropTypes.string.isRequired
+  };
+
+  state = {
+    clientId: keys.githubClientId,
+    clientSecret: keys.githubClientSecret,
+    count: 5,
+    sort: "created:asc",
+    repos: []
+  };
+
+  componentDidMount() {
+    const { username } = this.props;
+    const { count, sort, clientId, clientSecret } = this.state;
+
+    fetchGithub(username, count, sort, clientId, clientSecret)
+      .then(res => res.json())
+      .then(data => {
+        if (this.refs.githubRef) {
+          this.setState({ repos: data });
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    const { repos } = this.state;
+    return (
+      <div ref="githubRef" className="profile-github">
+        <hr />
+        <h3 className="mb-4">Latest Github Repos</h3>
+        {repos.length > 0 &&
+          repos.map(repo => (
+            <div key={repo.id} className="card card-body mb-2">
+              <div className="row">
+                <div className="col-md-6">
+                  <h4>
+                    <Link
+                      to={repo.html_url}
+                      className="text-info"
+                      target="_blank"
+                    >
+                      {repo.name}
+                    </Link>
+                  </h4>
+                  <p>{repo.description}</p>
+                </div>
+                <div className="col-md-6">
+                  <span className="badge badge-info mr-1">
+                    Stars: {repo.stargazers_count}
+                  </span>
+                  <span className="badge badge-secondary mr-1">
+                    Watchers: {repo.watchers_count}
+                  </span>
+                  <span className="badge badge-success">
+                    Forks: {repo.forks_count}
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    );
+  }
+}
+
+export default ProfileGithub;
